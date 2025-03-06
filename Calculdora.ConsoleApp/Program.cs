f65bfac9a62604b2a8636b5f0e79029e53433e6d
gsvsantos <gustavocnsantos02@gmail.com>
2025-03-06 17:07:26 -0300

    Add project files.

---
+++ Calculdora.ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Calculdora.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora Tabajara - 2025\n");
+
+            Console.Write("Digite o primeiro número: ");
+            string firstNumberString = Console.ReadLine();
+            int firstNumber = Convert.ToInt32(firstNumberString);
+
+            Console.Write("Digite o segundo número: ");
+            string secondNumberString = Console.ReadLine();
+            int secondNumber = Convert.ToInt32(secondNumberString);
+
+            int result = firstNumber + secondNumber;
+
+            Console.WriteLine("\nA soma desses número é: " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
