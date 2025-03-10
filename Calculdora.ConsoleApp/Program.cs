a2936ded06e3f8346737180d6d62adeda05eb8fc
gsvsantos <gustavocnsantos02@gmail.com>
2025-03-10 14:55:38 -0300

    feat: Adicionado opções de multiplicação e divisão. Adicionado suporte a casas decimais.

--- Calculdora.ConsoleApp/Program.cs
+++ Calculdora.ConsoleApp/Program.cs
@@ -13,6 +13,8 @@
 
                 Console.WriteLine("1 - Somar");
                 Console.WriteLine("2 - Subtrair");
+                Console.WriteLine("3 - Multiplicação");
+                Console.WriteLine("4 - Divisão");
                 Console.WriteLine("S - Sair");
 
                 Console.Write("\nEscolha uma opção: ");
@@ -20,41 +22,40 @@
                 string option = Console.ReadLine().ToUpper();
 
                 if (option == "S")
-                {
                     break;
-                }
 
                 Console.Write("Digite o primeiro número: ");
                 string firstNumberString = Console.ReadLine();
-                int firstNumber = Convert.ToInt32(firstNumberString);
+                decimal firstNumber = Convert.ToDecimal(firstNumberString);
 
                 Console.Write("Digite o segundo número: ");
                 string secondNumberString = Console.ReadLine();
-                int secondNumber = Convert.ToInt32(secondNumberString);
+                decimal secondNumber = Convert.ToDecimal(secondNumberString);
 
-                int result;
+                decimal result = 0;
 
                 // se / senao / senao se
                 if (option == "1")
-                {
                     result = firstNumber + secondNumber;
-                }
-                else
-                {
+
+                else if (option == "2")
                     result = firstNumber - secondNumber;
-                }
+
+                else if (option == "3")
+                    result = firstNumber * secondNumber;
+
+                else if (option == "4")
+                    result = firstNumber / secondNumber;
 
                 Console.WriteLine("----------------------------------");
-                Console.WriteLine("Resultado: " + result);
+                Console.WriteLine("Resultado: " + result.ToString("F2"));
                 Console.WriteLine("----------------------------------\n");
 
-                Console.Write("Deseja continuar? (S/N)");
+                Console.Write("Deseja continuar? (S/N): ");
                 string opcaoContinuar = Console.ReadLine().ToUpper();
 
                 if (opcaoContinuar == "N")
-                {
                     break;
-                }
             }
         }
     }
