0cb1d11bd791f76a94d8d7c2b426e191c1bca049
Gustavo Santos <gustavocnsantos02@gmail.com>
2025-03-17 15:19:54 -0300

    feat: Adicionado método de Tabuada a `Calculadora`. - Adicionado função 'GetHistory' a `Calculadora`.

--- Calculadora.ConsoleApp/Calculadora.cs
+++ Calculadora.ConsoleApp/Calculadora.cs
@@ -1,6 +1,10 @@
-﻿namespace Calculadora.ConsoleApp;
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Calculadora.ConsoleApp;
 public static class Calculadora
 {
+    public static int OperationsQuantity = 0;
+    public static string[] History = new string[200];
     public static decimal Addition(decimal firstNumber, decimal secondNumber)
     {
         decimal result = firstNumber + secondNumber;
@@ -20,5 +24,18 @@ public static class Calculadora
     {
         decimal result = firstNumber / secondNumber;
         return result;
+    }
+    public static void MultiplicationTable(int number)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            int result = number * i;
+            Console.WriteLine($"{number} x {i} = {result}");
+        }
+    }
+    public static void GetHistory(decimal firstNumber, decimal secondNumber, string signal, decimal result)
+    {
+        
+        History[OperationsQuantity] = $"{firstNumber} {signal} {secondNumber} = {Math.Round(result, 2)}";
     }
 }
